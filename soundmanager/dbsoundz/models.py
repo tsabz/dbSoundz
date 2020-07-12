04d6ebe236e5278d21c6948507edcda11ce8cf35
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-07-11 23:10:36 -0400

    created model

--- soundmanager/dbsoundz/models.py
+++ soundmanager/dbsoundz/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Sound(models.Model):
+    artist_name = models.CharField(max_length=100)
+    artist_genre = models.CharField(max_length=100)
+    artist_notes = models.CharField(max_length=500, blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
